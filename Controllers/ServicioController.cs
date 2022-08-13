98b263e88ce48b4f2e254084f20fd2a3a2d5fa7a
bfjose08 <bfjose26@gmail.com>
2022-08-13 00:23:09 -0500

    lista db, visualizacion de registros

---
+++ Controllers/ServicioController.cs
@@ -0,0 +1,30 @@
+﻿using CRUD_MVC_aspNet_framework_4.Models;
+using CRUD_MVC_aspNet_framework_4.Models.entidadb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_MVC_aspNet_framework_4.Controllers
+{
+    public class ServicioController : Controller
+    {
+        // GET: Servicio
+        public ActionResult Index()
+        {
+            List<EntidadServicio> listaServicio;
+            using(crudaspnetframewordEntities1 db =new crudaspnetframewordEntities1())
+            {
+                listaServicio = (from d in db.Servicio
+                                 select new EntidadServicio
+                                 {
+                                     id = d.id,
+                                     nombre = d.nombre,
+                                     direccion = d.direccion
+                                 }).ToList();
+            }
+            return View(listaServicio);
+        }
+    }
+}
---
+++ Models/Entidad/EntidadServicio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_MVC_aspNet_framework_4.Models.Entidad
+{
+    public class EntidadServicio
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+    }
+}
